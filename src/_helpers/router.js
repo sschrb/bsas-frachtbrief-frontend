bc30d569cf9574a923d2a48872190910fc39a2b1
schreiber <soerenschrb@gmail.com>
2020-08-26 11:41:26 +0200

    Initialisierung

---
+++ src/_helpers/router.js
@@ -0,0 +1,35 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+
+import HomePage from '../home/HomePage'
+import LoginPage from '../login/LoginPage'
+import RegisterPage from '../register/RegisterPage'
+import HistoryPage from '../history/HistoryPage'
+
+Vue.use(Router);
+
+export const router = new Router({
+  mode: 'history',
+  routes: [
+    { path: '/', component: HomePage },
+    { path: '/login', component: LoginPage },
+    { path: '/register', component: RegisterPage },
+    { path: '/history', component: HistoryPage },
+
+    // otherwise redirect to home
+    { path: '*', redirect: '/' }
+  ]
+});
+
+router.beforeEach((to, from, next) => {
+  // redirect to login page if not logged in and trying to access a restricted page
+  const publicPages = ['/login', '/register'];
+  const authRequired = !publicPages.includes(to.path);
+  const loggedIn = localStorage.getItem('user');
+
+  if (authRequired && !loggedIn) {
+    return next('/login');
+  }
+
+  next();
+})
---
+++ src/history/HistoryPage.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+       
+
+
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="navbar-brand" href="#"><router-link to="/">Frachtbrief</router-link></a>
+            </li>
+            <li class="nav-item">
+                <a class="navbar-brand" href="#"><router-link to="/history">Historie</router-link></a>
+            </li>            
+        </ul>
+    </div>
+    <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <router-link to="/login">Logout</router-link>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+        <p>
+            hier kommt history
+        </p>
+
+
+    </div>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex'
+
+export default {
+    computed: {
+        ...mapState({
+            account: state => state.account,
+            users: state => state.users.all
+        })
+    },
+    created () {
+        this.getAllUsers();
+    },
+    methods: {
+        ...mapActions('users', {
+            getAllUsers: 'getAll',
+            deleteUser: 'delete'
+        })
+    }
+};
+</script>
